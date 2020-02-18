20dd4a444e26085cb0ebcb25a214d32e41e7f347
Dawas <dawasry.h@hotmail.com>
2020-02-18 14:46:36 +0100

    Initial commit

---
+++ src/Entity/Reviews.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ReviewsRepository")
+ */
+class Reviews
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Book", inversedBy="reviews")
+     */
+    private $Book_id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBookId(): ?Book
+    {
+        return $this->Book_id;
+    }
+
+    public function setBookId(?Book $Book_id): self
+    {
+        $this->Book_id = $Book_id;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+}
